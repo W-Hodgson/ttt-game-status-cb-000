5563c81bae93b30618b1311a5a058a319ee96a7e
Will Hodgson <will.hodgson@current-rms.com>
2019-07-22 14:30:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,11 +11,9 @@ def won?(board)
       if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
         puts "X won with position #{win_combination}"
         return win_combination
-      elsif board[win_combination[0]] == "O" && board[win_combination[1]] =="O" && board[win_combination[2]] == "O"
+      else board[win_combination[0]] == "O" && board[win_combination[1]] =="O" && board[win_combination[2]] == "O"
         puts "O won with position #{win_combination}"
         return win_combination
-      else
-        return nil
       end
   end
 end
