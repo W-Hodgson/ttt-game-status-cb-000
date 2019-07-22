ba3defb27c0f53ba6fabb231ce9a8c560a27c469
Will Hodgson <will.hodgson@current-rms.com>
2019-07-22 14:27:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,10 +9,10 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
       if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-        puts "X won with position #{win_combination}}"
+        puts "X won with position #{win_combination}"
         win_combination
       elsif board[win_combination[0]] == "O" && board[win_combination[1]] =="O" && board[win_combination[2]] == "O"
-        puts "O won with position #{win_combination}}"
+        puts "O won with position #{win_combination}"
         win_combination
       else
         nil
