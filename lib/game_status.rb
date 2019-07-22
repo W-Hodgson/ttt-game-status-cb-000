35fd3411426e7768ad19ec1ada905e65d20e3930
Will Hodgson <will.hodgson@current-rms.com>
2019-07-22 14:49:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,17 +6,23 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
+# def won?(board)
+#   WIN_COMBINATIONS.each do |win_combination|
+#       if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+#         puts "X won with position #{win_combination}"
+#         return win_combination
+#       elsif board[win_combination[0]] == "O" && board[win_combination[1]] =="O" && board[win_combination[2]] == "O"
+#         puts "O won with position #{win_combination}"
+#         return win_combination
+#       else
+#         nil
+#       end
+#   end
+# end
+
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-      if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-        puts "X won with position #{win_combination}"
-        return win_combination
-      elsif board[win_combination[0]] == "O" && board[win_combination[1]] =="O" && board[win_combination[2]] == "O"
-        puts "O won with position #{win_combination}"
-        return win_combination
-      else
-        nil
-      end
+  WIN_COMBINATIONS.find do |win_combination|
+    board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" or board[win_combination[0]] == "O" && board[win_combination[1]] =="O" && board[win_combination[2]] == "O"
   end
 end
 
